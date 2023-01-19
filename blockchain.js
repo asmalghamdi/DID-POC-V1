6095a2b1ebbf9a6bd3cfc45c1687252e3547cfd0
Mohammed aljasser <justmo59@gmail.com>
2023-01-19 12:03:35 +0300

    feat: day 4

--- blockchain.js
+++ blockchain.js
@@ -1,6 +1,5 @@
 import { Block } from './block.js'
 import sha256 from "crypto-js/sha256.js"
-
 class Blockchain {
     list = []
     constructor() {
@@ -16,10 +15,12 @@ class Blockchain {
 
     addBlock(data) {
         const previousHash = this.getPerviousBlockHash()
-        // create an instance of a block 
-        this.list.push(new Block(data, previousHash, 0, this.getCurrentBlockHeight()))
-        console.log(this.list)
 
+        // create an instance of a block 
+        let block = new Block(data, previousHash, 0, this.getCurrentBlockHeight())
+        // mine the block
+        let MinedBlock = this.mine(block)
+        this.list.push(MinedBlock)
     }
 
     getCurrentBlockHeight() {
@@ -33,12 +34,58 @@ class Blockchain {
         console.log(this.list)
     }
 
-    // task 1 validate the blockchain
+    // validate the blockchain 
+    validateBlockchain() {
+        // loop through the list
+        for (let i = 1; i < this.list.length; i++) { // start from 1 because we don't need to validate the genesis block
+            // check if the current block is valid
+            if (!this.validateBlock(this.list[i])) { // if the block is not valid
+                return false // return false
+            }
+            // check if the current block's previous hash is equal to the previous block's hash
+            if (this.list[i].previousHash != this.list[i - 1].hash) { // if the previous hash is not equal to the previous block's hash
 
-    // task 2 validate the block
+                return false
+            }
+        }
+        return true
+    }
+    corruptBlockchain() {
+        //get a random block from the blockchain
+        let randomBlock = this.list[Math.floor(Math.random() * this.list.length)]
+        // corrupt the blockchain by changing the data of a block
+        randomBlock.data = "corrupted data"
 
-    // task 3 add the mining functionality
+    }
+    mine(block) {
+        for (let i = 0; i < 10_000_000; i++) { // loop through the numbers from 0 to 10_000_000
+            let magicHash = sha256(block.data + i).toString(); // calculate the hash of the block's data + the nonce
+            if (magicHash.startsWith("000")) { // if the hash starts with 000
+                block.nonce = i // set the nonce to the current number
+                block.hash = magicHash // set the hash to the magic hash
+                break // break the loop
+            }
+        }
+        return block
+    }
+
+    validateBlock(Block) {
+
+        // check if the block's hash is equal to the calculated hash
+        let hash = Block.hash // get the hash from the block
+
+        let calculatedHash = sha256(Block.data + Block.nonce) // calculate the hash from the block's data
+        return (hash == calculatedHash) // return the result
+    }
+
 }
 
 const blockchain = new Blockchain();
+// we add blocks to the blockchain
 blockchain.addBlock("second block", "0")
+blockchain.addBlock("third block", "0")
+blockchain.addBlock("fourth block", "0")
+blockchain.print();
+// blockchain.corruptBlockchain()
+// check if our blockchain is valid
+console.log("is the blockchain valid: " + blockchain.validateBlockchain())
